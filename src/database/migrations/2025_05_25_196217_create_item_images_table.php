9004fa15b52b8c0122e39bbc35c5b7f46e80bf98
matsuoka1985 <matsuoka1985@yahoo.co.jp>
2025-05-25 19:32:30 +0900

    item_imagesテーブル用マイグレーションファイル作成

---
+++ src/database/migrations/2025_05_25_196217_create_item_images_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_images', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_id')
+                ->constrained()
+                ->onDelete('cascade');
+
+            $table->string('image_url', 255);
+            $table->integer('sort_order')->default(1);
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_images');
+    }
+}
