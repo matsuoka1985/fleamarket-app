4f585bbf6ea5d2024c5de9b8c137720ed0d44be4
matsuoka1985 <matsuoka1985@yahoo.co.jp>
2025-06-01 10:04:12 +0900

    非同期によるいいね実装完了

--- src/app/Http/Controllers/LikeController.php
+++ src/app/Http/Controllers/LikeController.php
@@ -10,20 +10,25 @@ class LikeController extends Controller
     public function toggle(Item $item)
     {
         $user = auth()->user();
-
         $existingLike = $item->likes()->where('user_id', $user->id)->first();
 
         if ($existingLike) {
             $existingLike->delete();
+            $liked = false;
         } else {
             $item->likes()->create([
                 'user_id' => $user->id,
                 'created_at' => now(),
             ]);
+            $liked = true;
         }
 
-        return back();
+        return response()->json([
+            'liked' => $liked,
+            'count' => $item->likes()->count(),
+        ]);
     }
+
 
     /**
      * Display a listing of the resource.
